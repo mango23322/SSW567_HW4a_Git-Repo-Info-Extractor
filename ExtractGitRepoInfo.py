56ab3b9cafc77c5c6399f64a1c98b65829700d00
mango23322 <mango23322@gmail.com>
2021-10-09 12:58:57 -0500

    updated hw5

--- ExtractGitRepoInfo.py
+++ ExtractGitRepoInfo.py
@@ -11,9 +11,9 @@ import json
 
 ############################################
 
-def extractGitData(user):
+def extractUserData(user):
     """
-    when given a username, return the users repos and the # of commits for each repo
+    when given a username, return a list of the users repos and the # of commits for each repo
     """
     invalidInput = 'Invalid Input'
     noUser = "User does not exist"
@@ -21,7 +21,7 @@ def extractGitData(user):
     repos_dict = {} # dict of repos for current user and commits for each repo
 
     # verify that user name is a string
-    if not(isinstance(user,str)):
+    if not isinstance(user,str):
         return invalidInput
 
     # web address with info on users repos
@@ -40,21 +40,35 @@ def extractGitData(user):
 
         repos_dict[repo_info['name']] = 0 # add the name of the current repo to the dict
 
+    return(repos_dict)
+
+def extractCommitData(user, repos_dict):
     for name in repos_dict:
         # web address that contains the commits for each repo
         repo_address = "https://api.github.com/repos/" + user + "/" + name +"/commits"
         current_repo = requests.get(repo_address) # get the data from the web address
-        count = current_repo.text.count('parents') # count the # of commits for the current repo
-        repos_dict[name] = count # add the count to the dictionary
+        count = current_repo.json()
+        repos_dict[name] = len(count) # add the count to the dictionary
 
-    return(repos_dict)
-"""
+    return repos_dict
+
 #main
+
+invalidInput = 'Invalid Input'
+noUser = "User does not exist"
+
+
 while True:
     username = input("Enter the user name: ")
-    data = extractGitData(username)
-    #data = extractGitData('mango23322')
+    data = extractUserData(username) # returns a dict with the usernames repositories
 
-    for i in data:
-        print(i + " : " + str(data[i]))
-"""
+    # if the data returned has any abnormal data, print out the proper message
+    if data == invalidInput:
+        print(invalidInput)
+    elif data == noUser:
+        print(noUser)
+    else:
+        data = extractCommitData(username, data) # find the # of commits for each repo, add the value to the dict, return the updated dict
+        for i in data:
+            print(i + " : " + str(data[i])) # print the dict key and the value for the users repos and commits
+
--- TestExtractGitRepoInfo.py
+++ TestExtractGitRepoInfo.py
@@ -4,8 +4,10 @@ The primary goal of this file is to demonstrate a simple unittest implementation
 """
 
 import unittest
+import json
+from unittest import mock
 
-from ExtractGitRepoInfo import extractGitData
+from ExtractGitRepoInfo import extractUserData, extractCommitData
 
 # This code implements the unit test functionality
 # https://docs.python.org/3/library/unittest.html has a nice description of the framework
@@ -13,13 +15,25 @@ from ExtractGitRepoInfo import extractGitData
 class TestExtractGitRepoInfo(unittest.TestCase):
     # define multiple sets of tests as functions with names that begin
 
-# my repo, # of repos
-# my repo, specific repo / number of commits
-# bad username
-# empty repo?
-# not a string
+    def mockResponse(self, json_data):
+        """returns the mock response for the json data"""
+        mock_response = mock.Mock()
+        mock_response.json = mock.Mock(return_value = json_data)
+        return mock_response
+
+    @mock.patch('requests.get')
+    def testGetRepoNames(self, mockedReq):
+        """test retreiving the get repos, ensure that the correct number of repos are retrieved (2)"""
+        mockedReq.return_value = self.mockResponse(json_data = [{"name":"repo1"},{"name": "repo2"}])
+        self.assertEqual(len(extractUserData("user")),2)
 
 
+    @mock.patch('requests.get')
+    def testGetCommits(self, mockedReq):
+        mockedReq.return_value = self.mockResponse(json_data = [{"parents":"1"},{"parents":"2"},{"parents":"3"}])
+        x = extractCommitData("user",{"repo1" : 0})
+        print (x)
+"""
     # the repo 'squareD' has two commits and is not being updated so it should be static
     # so I am only focusing on this repo for this test
     def testSquareDHasTwoCommits(self): 
@@ -34,7 +48,7 @@ class TestExtractGitRepoInfo(unittest.TestCase):
  
     def testNotAString(self): 
         self.assertEqual(extractGitData(1234),"Invalid Input")  
-
+"""
 if __name__ == '__main__':
     print('Running unit tests')
     unittest.main()
